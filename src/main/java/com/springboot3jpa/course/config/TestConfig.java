fea42dff1a99217c68a4ad4e061ce724b2079cea
caiomunhoz <caio.gmunhoz@gmail.com>
2023-12-07 15:31:04 -0300

    OrderStatus enum

--- src/main/java/com/springboot3jpa/course/config/TestConfig.java
+++ src/main/java/com/springboot3jpa/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.springboot3jpa.course.entities.Order;
 import com.springboot3jpa.course.entities.User;
+import com.springboot3jpa.course.entities.enums.OrderStatus;
 import com.springboot3jpa.course.repositories.OrderRepository;
 import com.springboot3jpa.course.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Ryan Gosling", "ryang@gmail.com", "999999999", "password");
 		User u2 = new User(null, "Ana de Armas", "ana@outlook.com", "111111111", "password");
 		
-		Order o1 = new Order(null, Instant.parse("2023-12-01T09:32:27Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2023-12-04T20:02:01Z"), u1);
-		Order o3 = new Order(null, Instant.parse("2023-12-07T14:57:12Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2023-12-01T09:32:27Z"), OrderStatus.DELIVERED, u1);
+		Order o2 = new Order(null, Instant.parse("2023-12-04T20:02:01Z"), OrderStatus.CANCELLED, u1);
+		Order o3 = new Order(null, Instant.parse("2023-12-07T14:57:12Z"), OrderStatus.PAID, u2);
 		
 		userRepo.saveAll(Arrays.asList(u1, u2));
 		orderRepo.saveAll(Arrays.asList(o1, o2, o3));
