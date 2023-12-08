87d09385ded4ba569d8577a6033d96b2a9b9198b
caiomunhoz <caio.gmunhoz@gmail.com>
2023-12-08 14:50:47 -0300

    Subtotal and total methods

--- src/main/java/com/springboot3jpa/course/entities/Order.java
+++ src/main/java/com/springboot3jpa/course/entities/Order.java
@@ -98,6 +98,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		for (OrderItem orderItem : items) {
+			total += orderItem.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
